b2fe7dbdbc4a0dcdd08d0a0b23de4b0ce69b68f8
jletteboer <john@networkx.nl>
2022-01-31 17:26:50 +0100

    Updates in notebook and new video

--- Project/notebooks/functions.py
+++ Project/notebooks/functions.py
@@ -1,14 +1,14 @@
-import subprocess                       # For spawning new processes
 import getpass                          # For getting username and password
-import requests                         # For getting and posting HTTP/1.1 requests to URLs
+import requests                         # For getting and posting HTTP requests to URLs
 import urllib3                          # For disabling SSL warnings
-import os                               # For getting filesize
+import os                               # For getting filesize, checking directories an files
 from tqdm.notebook import tnrange       # For showing progressbar
 import IP2Location                      # IP Geolocation Python Library for getting location
 import pandas as pd                     # Import pandas for dataframes and series
 import matplotlib.pyplot as plt         # For making plots
 import matplotlib.patches as mpatches   # Customizing legends
 import seaborn as sns                   # Seaborn visualization package
+from scipy import stats                 # For using statistical functions
 
 ## Disable warnings about certificates
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -19,52 +19,13 @@ CEND = '\33[0m'
 RED = '\033[1;91;1m'
 BOLD = '\033[1m'
 
-
-def data_groupby(df, by, top=15):
-    '''This function returns a groupedby dataframe
-    '''
-    ## Create new dataset based on given inputs, sort values and return given top
-    data = pd.DataFrame(df.groupby(by).size().sort_values(ascending=False)[:top], columns=['count'])
-    
-    return data
+## Setting static color 
+static_color = '#38a3d8'
 
 
-def country_dropdown(df, Country, Top):
-    '''This function wil be used for widget interaction of chosen country and returns two barplots.
-    '''
-    fig, axs = plt.subplots(ncols=2, figsize=(15,10))
-    static_color = '#38a3d8'
-    edge_color = 'black'
-    selected_color = 'orange'
-    legend_labels = mpatches.Patch(facecolor=selected_color, label='Selected Country', edgecolor=edge_color)
-    
-    ## Create dataframe with top countries
-    data_country = data_groupby(df=df, by='country_short', top=Top)
-    
-    if Country == '# All':
-        data = data_groupby(df=df, by='city', top=Top)
-        clrs = [static_color for x in data_country['count']]
-        sns.barplot(data=data, x='count', y=data.index, color=static_color, edgecolor=edge_color, 
-                    ax=axs[0]).set(title=f'Hits by top {Top} cities', xscale='log')
-
-    else:
-        data = data_groupby(df=df[df['country_short'] == Country], by='city', top=Top)
-        clrs = [selected_color if (x == data_country.loc[Country][0]) else static_color for x in data_country['count'] ]
-        sns.barplot(data=data, x='count', y=data.index, color=static_color, edgecolor=edge_color, 
-                    ax=axs[0]).set(title=f'Hits from top {Top} cities of {Country}', xscale='log')
-     
-    ## Plot always countries
-    sns.barplot(data = data_country, x='count', y=data_country.index, 
-                palette=clrs, edgecolor=edge_color, ax=axs[1])\
-                .set(title=f'Hits from top {Top} countries', xscale='log');
-    
-    axs[1].legend(handles=[legend_labels]) 
-    
-    ## Function will return fig automatticly, if I return fig too two figure will be created twice
-
-    
-def yes_or_no(question, default_no=True):
-    """Ask a yes/no question via input() and return False or True.
+def yesNo(question, default_no=True):
+    """Ask a yes/no question, give input as answer and return False or True.
+    Function will only check first character in answer
     
     Args:
         question   : string  : What is the question you want to be answered with yes or no. 
@@ -75,7 +36,7 @@ def yes_or_no(question, default_no=True):
         Returns value is True for "yes" or False for "no".
         
     Example:
-        yes_or_no("Do you want to download the file via Splunk API?")
+        yesNo("Do you want to download the file via Splunk API?")
     """
     
     ## Define choises and defaut answers
@@ -94,13 +55,14 @@ def yes_or_no(question, default_no=True):
         return False if default_no else True
 
 
-def download_data(search_user, app, searchname, output_file, output_type = 'csv', host = 'localhost'):
+def downloadData(search_user, app, searchname, path, output_file, output_type = 'csv', host = 'localhost'):
     """Download data from Splunk API.
 
     Args:
         search_user : string : Splunk search owner
         app         : string : Splunk App name where search belongs to
         searchname  : string : Savedsearch name
+        path        : string : Path to save file e.q. '../data/' 
         output_file : string : Where to output the events (including path), e.q. data/test.csv
         output_type : string : Which type of file for output (csv, json, raw, xml)
         host        : string : Hostname of Splunk server
@@ -109,11 +71,11 @@ def download_data(search_user, app, searchname, output_file, output_type = 'csv'
         An file with the downloaded events of the search.
 
     Example:
-        download_data('user1', 'search', 'webserver_logging_search', 'webserver_log', 'csv', 'localhost')
+        downloadData('user1', 'search', 'webserver_logging_search', '../data/', 'webserver_log', 'csv', 'localhost')
     """
     
     ## Ask user if we must downloading the file from API
-    if yes_or_no("Do you want to download new data?"):
+    if yesNo("Do you want to download new data?"):
         
         ## Ask for login credentials for Splunk
         print(f'Enter Splunk credentials for downloading new data')
@@ -145,8 +107,16 @@ def download_data(search_user, app, searchname, output_file, output_type = 'csv'
             print(f'HTTP Status is {GREEN}OK{CEND}')
             print(f'Trying to download data from {host} via API')
             
+            ## Check if path exists
+            dir_check = os.path.isdir(path)
+            if not dir_check:
+                os.makedirs(path)
+                print(f'Directory {RED}{path} not exists{CEND}, created directory')
+            else:
+                print(f'Directory {GREEN}{path} already exists{CEND}.')   
+            
             ## Save file to disk
-            filename = output_file + "." + output_type
+            filename = path + output_file + "." + output_type
             with open(filename, "w") as f:
                 f.write(response.text)
                   
@@ -172,7 +142,7 @@ def download_data(search_user, app, searchname, output_file, output_type = 'csv'
         print(f'{RED}New data will not be downloaded (︶︿︶){CEND}')
         
         
-def get_location(ip):
+def getLocation(ip):
     '''Get GEO location of given IP address
     
     Args:
@@ -189,7 +159,7 @@ def get_location(ip):
             longitude     : City longtitude of the IP address.
     
     Example:
-            get_location("172.217.168.195")
+            getLocation("172.217.168.195")
     '''
     ## Cache the database into memory to accelerate lookup speed.
     ## WARNING: Please make sure your system have sufficient RAM to use this feature.
@@ -212,7 +182,7 @@ def get_location(ip):
     return locations
 
 
-def http_status_class(series: str):
+def httpStatusClass(series: str):
     '''Convert HTTP status code to HTTP Class
     
     Args:
@@ -222,7 +192,7 @@ def http_status_class(series: str):
         Returns the class of a HTTP status code.
         
     Example:
-        http_status_class(df['http_status'])
+        httpStatusClass(df['http_status'])
     '''
     if series.startswith('1'):
         return "Informational"
@@ -235,13 +205,94 @@ def http_status_class(series: str):
     if series.startswith('5'):
         return "Server errors"
 
+def dataGroupby(df, by, top=15):
+    '''This function returns a topN, counted, groupedby dataframe
+    
+    Args:
+        df  : DataFrame : Dataframe
+        by  : string    : Wich column must be grouped and counted
+        top : int       : How many values will be returned, topN
+        
+    Returns:
+        Return dataframe with topN of grouped column
+        
+    Example:
+        dataGroupby(dataframe, country)
+    '''
+    ## Create new dataset based on given inputs, count, sort values and return given top
+    data = pd.DataFrame(df.groupby(by).size().sort_values(ascending=False)[:top], columns=['count'])
+    
+    return data
 
-def pltMovingAverage(timeseries, window, plotBounds=False, plotOutlier=False):
+
+def countryDropdown(df, Country, Top):
+    '''This function wil be used for widget interaction of chosen country and returns two barplots with Country and City
+    
+    Args:
+        df      : DataFrame : Dataframe with country and city colmns
+        Country : string    : Country list that will show in the dropdown
+        Top     : int       : Number op ow many values wil be returned
+    
+    Returns:
+        Two barplots of Country and city with dropdown for Country choice and slider for topN.
+        
+    Example:
+        ## Setting top for creating country_list as input for interaction
+        set_top = 20
+
+        ## reate country_list for input ineraction
+        country_list = data_groupby(df=df_web_ts, by='country_short', top=set_top).index.tolist()
+
+        ## Add option "# All" for all Countries
+        country_list.append('# All')
+
+        ## Sort list
+        country_list.sort()
+
+        ## Adding dropdown of country_list and slicer for setting the topN
+        ## For dataframe we used fixed, so no interact (e.q. dropdown, slicer) will be created
+        widgets.interact(countryDropdown, df=fixed(df_web_ts[['country_short','city']]), 
+                         Country=country_list, Top=widgets.IntSlider(min=1,max=set_top,value=15));
+    '''
+    
+    ## Create figure, legend for selected item, and setting colors
+    fig, axs = plt.subplots(ncols=2, figsize=(15,10))
+    edge_color = 'black'
+    selected_color = 'orange'
+    legend_labels = mpatches.Patch(facecolor=selected_color, label='Selected Country', edgecolor=edge_color)
+    
+    ## Create dataframe with top countries
+    data_country = dataGroupby(df=df, by='country_short', top=Top)
+    
+    if Country == '# All':
+        data = dataGroupby(df=df, by='city', top=Top)
+        clrs = [static_color for x in data_country['count']]
+        sns.barplot(data=data, x='count', y=data.index, color=static_color, edgecolor=edge_color, 
+                    ax=axs[0]).set(title=f'Hits by top {Top} cities', xscale='log')
+
+    else:
+        data = dataGroupby(df=df[df['country_short'] == Country], by='city', top=Top)
+        clrs = [selected_color if (x == data_country.loc[Country][0]) else static_color for x in data_country['count'] ]
+        sns.barplot(data=data, x='count', y=data.index, color=static_color, edgecolor=edge_color, 
+                    ax=axs[0]).set(title=f'Hits from top {Top} cities of {Country}', xscale='log')
+     
+    ## Plot always countries
+    sns.barplot(data = data_country, x='count', y=data_country.index, 
+                palette=clrs, edgecolor=edge_color, ax=axs[1])\
+                .set(title=f'Hits from top {Top} countries', xscale='log');
+    
+    axs[1].legend(handles=[legend_labels]) 
+    
+    ## Function will return fig automatticly, if I return fig too two figure will be created twice
+
+    
+def pltMovingAverage(timeseries, window, measure='mean', madType='mean', plotBounds=False, plotOutlier=False):
     """Function for plotting timeseries dataframe, with lower, upper bounds and outliers
     
     Args:
-        timeseries  : dataframe : Dataframe with timeseries
+        timeseries  : dataframe : Dataframe with timeseries and values
         window      : int       : Rolling window size
+        measure     : string    : Measures of Central Tendency, mean, median 
         plotBounds  : bool      : Show confidence intervals
         plotOutlier : bool      : Show anomalies
         
@@ -251,9 +302,17 @@ def pltMovingAverage(timeseries, window, plotBounds=False, plotOutlier=False):
     Example:
         pltMovingAverage(ts, 7, plotBounds=True, plotOutlier=True)
     """
+    valid = {'mean', 'median'}
+    if measure not in valid:
+        raise ValueError(f'Results: measure must be one of {valid}')
+    elif madType not in valid:
+        raise ValueError(f'Results: madType must be one of {valid}')
     
-    ## Create rolling mean based on input
-    rolling_mean = timeseries.rolling(window=window).mean()
+    ## Create rolling measure based on input
+    if measure == 'mean':
+        rolling_measure = timeseries.rolling(window=window).mean()
+    elif measure == 'median':
+        rolling_measure = timeseries.rolling(window=window).median()
     
     ## Setting figure size
     plt.figure(figsize=(20, 8))
@@ -262,17 +321,22 @@ def pltMovingAverage(timeseries, window, plotBounds=False, plotOutlier=False):
     plt.title(f'Moving average with a window size of {window}')
     
     ## Plot rolling mean and actual values
-    plt.plot(rolling_mean, "g--", label="Rolling mean")
+    plt.plot(rolling_measure, "g--", label=f'Moving {measure}')
     plt.plot(timeseries[window:], label="Actual values", color=static_color)
     
     ## If plotBounds is True
     if plotBounds:
-        ## Mean absolute deviation (MAD) measures 
-        ts_mad = timeseries.mad().item()
+        ## Check what madType is
+        if madType == 'mean':
+            ## Mean absolute deviation 
+            ts_mad = timeseries.mad().item()
+        elif madType == 'median':
+            ## Median absolute deviation 
+            ts_mad = stats.median_abs_deviation(timeseries)[0]
         
         ## Create lower and upper bounds
-        lower_bound = rolling_mean - ts_mad
-        upper_bound = rolling_mean + ts_mad
+        lower_bound = rolling_measure - ts_mad
+        upper_bound = rolling_measure + ts_mad
         
         ## Plot lower and upper bounds
         plt.plot(upper_bound, "m:", markersize=1.5, label="Lower bound / Upper bound")
@@ -292,7 +356,7 @@ def pltMovingAverage(timeseries, window, plotBounds=False, plotOutlier=False):
         
             ## Plot outliers
             plt.plot(outliers, "ro", markersize=5)
-            plt.title(f'Moving average with a window size of {window}\n Number of outliers is {n_outliers}')
+            plt.title(f'Moving {measure} with a window size of {window}\n Number of outliers is {n_outliers}')
     
     ## Plot legend
     plt.legend()
---
+++ README.md
@@ -0,0 +1,13 @@
+# Programing for Data Science
+
+This is a Project for PHBO AI at HU module Programming for Datascience in Python
+
+There is a requirements.txt created of the Python environment. 
+
+```bash
+$ conda list -e > requirements.txt
+```
+
+```bash
+$ conda create --name p4ds --file requirements.txt
+```
